9b9bd82bb2473129596d3ac19973ef377e609242
HAYGEE <143488367+Haygee2@users.noreply.github.com>
2026-02-19 15:03:10 +0100

    Update Supabase URL and anon key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'PASTE_YOUR_PROJECT_URL'
-const supabaseAnonKey = 'PASTE_YOUR_ANON_KEY'
+const supabaseUrl = 'https://skqbkmiljimlbmlxjjdz.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNrcWJrbWlsamltbGJtbHhqamR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1MDIyNzQsImV4cCI6MjA4NzA3ODI3NH0.qg1AO4dje-sy5ByDERjEXYhRXpzbGvavWz-QwqCDXG4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
